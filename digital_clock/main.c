cb76a67c674176c7a4d1f043b76262bc94f39015
Fábián Gábor <info@fabiangabor.com>
2019-09-22 19:09:17 +0200

    add : delimiter

--- digital_clock/main.c
+++ digital_clock/main.c
@@ -102,35 +102,26 @@ void digits(int h, int min, int sec)
             digi_time[5][i][j] = digit[sec % 10][i][j];
         }
 
-/*
-        for (int x=0;x<5;x++) {
-            for (int y=0;y<3;y++)
-            {
-                printf("%d", digi_time[0][x][y]);
-            }
-            printf("\n");
-        }
-*/
-
-
         for (int x = 0; x < 5; x++)
         {
             for (int i=0;i<=5;i++)
             {
+                if ((i>0) && (i%2==0))
+                {
+                    if (x%2==1)
+                       printf(" o  ");
+                    else
+                        printf("    ");
+                }
                  for (int y = 0; y < 3; y++)
                     if (digi_time[i][x][y]!=0)
-                        printf("%c", 248);
+                        printf("%c", 219);
                     else
                         printf(" ");
-
-                printf("   ");
+                 printf(" ");
             }
             printf("\n");
         }
-
-
-
-
     return;
 }
 
@@ -138,7 +129,6 @@ void current_time()
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    //printf("%d %d %d \n", tm.tm_hour, tm.tm_min, tm.tm_sec);
     digits(tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 }
