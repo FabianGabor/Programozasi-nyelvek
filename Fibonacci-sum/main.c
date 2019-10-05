a97f06c1fca0682bee72a1b1866d2a6b89d84b02
Fábián Gábor <info@fabiangabor.com>
2019-10-05 17:51:23 +0200

    Sequential

--- Fibonacci-sum/main.c
+++ Fibonacci-sum/main.c
@@ -6,22 +6,16 @@ int main()
 {
     double start = omp_get_wtime();
     double runTime;
-    int i,n = 99999;
+    int i,n = 99;
     unsigned long long fib, fib_prev=1, fib_prev2=0, sum=0;
 
-    //printf("n=");
-    //scanf("%d", &n);
-
     printf("1 : 0\n2 : 1\n");
-    // 7.1s vs
-//#pragma omp parallel for num_threads(8)
+
     for (i=3; i<=n; i++) {
         fib = fib_prev + fib_prev2;
         fib_prev2 = fib_prev;
         fib_prev = fib;
         sum = sum + fib;
-
-  //      #pragma omp critical
         printf("%d : %llu\n", i, fib);
     }
 
