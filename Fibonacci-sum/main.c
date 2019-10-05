97e4a6257ea45df169cf91533abadcf30fcbd010
Fábián Gábor <info@fabiangabor.com>
2019-10-05 14:35:41 +0200

    fibonacci

--- Fibonacci-sum/main.c
+++ Fibonacci-sum/main.c
@@ -1,19 +1,115 @@
 #include <stdio.h>
+#include <time.h>
+#include <omp.h>
+
+unsigned long long f(unsigned long long n) {
+    if (n<2) return n;
+    else return ( f(n-1) + f(n-2) );
+}
 
 int main()
 {
-    int i, n, fib, fib_prev=1, fib_prev2=0, sum=0;
-    printf("n=");
-    scanf("%d", &n);
-    printf("0, 1, ");
-    for (i=0; i<n-2; i++) {
-        fib = fib_prev + fib_prev2;
-        fib_prev2=fib_prev;
-        fib_prev=fib;
-        sum = sum + fib;
+    double start = omp_get_wtime();
+    double runTime;
 
-        printf((i<n-3)?"%d, ":"%d", fib);
+    //int i;
+    unsigned long long i,n;
+    //int sum=0;
+    unsigned long long fib;
+
+    printf("n=");
+    scanf("%llu", &n);
+
+    #pragma omp parallel num_threads(7)
+    {
+    #pragma omp critical
+        {
+
+                for (i=0; i<n; i++) {
+
+                    fib = f(i);
+                    //sum += fib;
+                    printf("%llu : ", i);
+                    printf((i<n-1)?"%llu, \n":"%llu\n",fib);
+
+                }
+        }
     }
-    printf("\nSum = %d, ", sum);
+
+
+
+
+
+    /*
+    for (i=0; i<n; i++) {
+
+        fib = f(i);
+        //sum += fib;
+        printf("%d : ", i);
+        printf((i<n-1)?"%llu, \n":"%llu\n",fib);
+
+    }
+    */
+    //printf("\nSum = %d\n", sum);
+
+    double end = omp_get_wtime();
+    runTime = end - start;
+    printf("This machine calculated all fibonacci numbers up to %llu in %lf seconds\n", n, runTime);
     return 0;
 }
+
+
+/*
+int main()
+{
+    clock_t start, end;
+    double runTime;
+    start = clock();
+
+    int n, sum=0;
+    unsigned long long fib;
+    printf("n=");
+    scanf("%d", &n);
+
+    for (int i=0; i<n; i++) {
+        fib = f(i);
+        //sum += fib;
+        printf("%d : ", i);
+        printf((i<n-1)?"%llu, \n":"%llu\n",fib);
+
+    }
+    //printf("\nSum = %d\n", sum);
+
+    end = clock();
+    runTime = (end - start) / (double) CLOCKS_PER_SEC;
+    printf("This machine calculated all fibonacci numbers up to %d in %g seconds\n", n, runTime);
+    return 0;
+}*/
+/*
+int main()
+{
+    int i,j,n,fibnum[100];
+    printf("Enter the Limit of the series: ");
+    scanf("%d",&n);
+    #pragma omp parallel num_threads(2)
+    {
+    #pragma omp critical
+        {
+            if(omp_get_thread_num()==0)
+            {
+                printf("\nThere are total %d threads\n",omp_get_num_threads());
+                printf("\nThread no %d is the generating thread\n",omp_get_thread_num());
+                for(i=0;i<n;i++)
+                    fibnum[i]=f(i);
+            }
+            else
+            {
+                printf("Thread no %d is the printing thread\n",omp_get_thread_num());
+                for(j=0;j<n;j++)
+                    printf("%d\t",f[j]);
+                printf("\n");
+            }
+        }
+    }
+}
+*/
